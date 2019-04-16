6787365eebe6e7a51710b9716f86bdfdcb5215fd
climbRyokawa1 <s.tempter.e2411@gmail.com>
2019-04-17 04:58:12 +0900

    PinBall コミット
    得点表示

---
+++ Assets/Lesson5/BallController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+
+	private float visiblePosZ = -6.5f;
+
+	//ゲームオーバを表示するテキスト
+	private GameObject gameoverText;
+
+	private GameObject scoreText;
+
+	private int score;
+
+
+	void Start () {
+		
+		this.gameoverText = GameObject.Find ("GameOverText");
+
+		this.scoreText = GameObject.Find ("ScoreText");
+
+		this.score = 0;
+
+		SetScore ();
+
+	}
+
+	void SetScore()
+	{
+		this.scoreText.GetComponent<Text> ().text = "Score:"+score;
+	}
+
+
+	void Update () {
+		
+		if (this.transform.position.z < this.visiblePosZ) {
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "LargeStarTag") {
+			this.score += 5;
+			SetScore ();
+		} else if (col.gameObject.tag == "SmallCloudTag") {
+			this.score += 20;
+			SetScore ();
+		} else if (col.gameObject.tag == "LargeCloudTag") {
+			this.score += 50;
+			SetScore ();
+		} else if (col.gameObject.tag == "SmallStarTag") {
+			this.score += 10;
+			SetScore ();
+		}
+}
+}
